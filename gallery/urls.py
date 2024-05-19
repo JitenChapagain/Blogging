74ac5e1316d7a0ac544aaf4cd41c12ab1f50ac6d
JitenChapagain <jitenchapagain45@gmail.com>
2024-05-19 13:19:09 +0545

    final

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
  
 urlpatterns = [
     path('/home', views.home, name='home'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('<int:pk>/', views.product_detail, name='product_detail'),
     path('<int:pk>/edit/', views.edit_product, name='edit_product'),
     path('<int:pk>/delete/', views.delete_product, name='delete_product'),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
